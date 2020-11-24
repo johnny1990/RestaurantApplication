f4d790cf346d1d0941c4eb18f8a17a1b8de6fb6e
Man Ioan <man_ioan@live.com>
2020-11-24 12:24:29 +0200

    Add meal entity  & correct issues & pagging

---
+++ RestaurantApp.Web/Controllers/MealsController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RestaurantApp.Entities;
+using RestaurantApp.Web.Models;
+
+namespace RestaurantApp.Web.Controllers
+{
+    [Authorize]
+    public class MealsController : Controller
+    {
+        private readonly RestaurantDbContext _context;
+
+        public MealsController(RestaurantDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Meals
+        public async Task<IActionResult> Index(int? pageNumber)
+        {
+            int pageSize = 7;
+            return View(await PagedList<Meal>.CreateAsync(_context.Meals.AsNoTracking(), pageNumber ?? 1, pageSize));
+        }
+
+        // GET: Meals/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var meal = await _context.Meals
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (meal == null)
+            {
+                return NotFound();
+            }
+
+            return View(meal);
+        }
+
+        // GET: Meals/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Meals/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Comments")] Meal meal)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(meal);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(meal);
+        }
+
+        // GET: Meals/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var meal = await _context.Meals.FindAsync(id);
+            if (meal == null)
+            {
+                return NotFound();
+            }
+            return View(meal);
+        }
+
+        // POST: Meals/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Comments")] Meal meal)
+        {
+            if (id != meal.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(meal);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MealExists(meal.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(meal);
+        }
+
+        // GET: Meals/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var meal = await _context.Meals
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (meal == null)
+            {
+                return NotFound();
+            }
+
+            return View(meal);
+        }
+
+        // POST: Meals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var meal = await _context.Meals.FindAsync(id);
+            _context.Meals.Remove(meal);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MealExists(int id)
+        {
+            return _context.Meals.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ RestaurantApp.Web/Models/PagedList.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantApp.Web.Models
+{
+    public class PagedList<T> : List<T>
+    {
+        public int PageIndex { get; private set; }
+        public int TotalPages { get; private set; }
+
+        public PagedList(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            this.AddRange(items);
+        }
+
+        public bool HasPreviousPage
+        {
+            get
+            {
+                return (PageIndex > 1);
+            }
+        }
+
+        public bool HasNextPage
+        {
+            get
+            {
+                return (PageIndex < TotalPages);
+            }
+        }
+
+        public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+            return new PagedList<T>(items, count, pageIndex, pageSize);
+        }
+    }
+}

6913fec0fb800533318ee043a3160403d6ba5011
Man Ioan <man_ioan@live.com>
2020-11-29 13:14:00 +0200

    Add menu controller&views entity.

---
+++ RestaurantApp.Web/Controllers/MenusController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RestaurantApp.Entities;
+using RestaurantApp.Web.Models;
+
+namespace RestaurantApp.Web.Controllers
+{
+    [Authorize]
+    public class MenusController : Controller
+    {
+        private readonly RestaurantDbContext _context;
+
+        public MenusController(RestaurantDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Menus
+        public async Task<IActionResult> Index()
+        {
+            var restaurantDbContext = _context.Menus.Include(m => m.Chef);
+            return View(await restaurantDbContext.ToListAsync());
+        }
+
+        // GET: Menus/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var menu = await _context.Menus
+                .Include(m => m.Chef)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+
+            return View(menu);
+        }
+
+        // GET: Menus/Create
+        public IActionResult Create()
+        {
+            ViewData["ChefId"] = new SelectList(_context.Chefs, "Id", "Name");
+
+            List<string> mlist = new List<string>();
+            ViewBag.Meal = new SelectList(_context.Meals, "Id", "Name");
+            foreach (var item in ViewBag.Meal)
+            {
+                mlist.Add(item.Text + ", ");
+            }
+            ViewBag.Meals = mlist;
+
+            return View();
+        }
+
+        // POST: Menus/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,ChefId,Price,Meals")] Menu menu)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(menu);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ChefId"] = new SelectList(_context.Chefs, "Id", "Name", menu.ChefId);
+            return View(menu);
+        }
+
+        // GET: Menus/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var menu = await _context.Menus.FindAsync(id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+            ViewData["ChefId"] = new SelectList(_context.Chefs, "Id", "Name", menu.ChefId);
+            return View(menu);
+        }
+
+        // POST: Menus/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,ChefId,Price,Meals")] Menu menu)
+        {
+            if (id != menu.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(menu);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MenuExists(menu.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ChefId"] = new SelectList(_context.Chefs, "Id", "Name", menu.ChefId);
+            return View(menu);
+        }
+
+        // GET: Menus/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var menu = await _context.Menus
+                .Include(m => m.Chef)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+
+            return View(menu);
+        }
+
+        // POST: Menus/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var menu = await _context.Menus.FindAsync(id);
+            _context.Menus.Remove(menu);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MenuExists(int id)
+        {
+            return _context.Menus.Any(e => e.Id == id);
+        }
+    }
+}

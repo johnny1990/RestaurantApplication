3bf26142a5e246e7562ef56bd5752d4212f2fa89
Man Ioan <man_ioan@live.com>
2020-12-14 22:41:30 +0200

    Adjust changes

--- RestaurantApp.Web/Controllers/ShoppingCartController.cs
+++ RestaurantApp.Web/Controllers/ShoppingCartController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using RestaurantApp.Entities;
 using RestaurantApp.Web.Helpers;
 using RestaurantApp.Web.Models;
@@ -10,6 +11,7 @@ using System.Threading.Tasks;
 
 namespace RestaurantApp.Web.Controllers
 {
+    [Authorize]
     public class ShoppingCartController : Controller
     {
         private readonly RestaurantDbContext _context;

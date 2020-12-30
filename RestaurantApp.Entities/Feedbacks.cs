60c2886f5ce9c0eedd83e5756cce4cd3e977a344
Man Ioan <man_ioan@live.com>
2020-12-30 13:14:40 +0200

    Add page for customers feedback.

--- RestaurantApp.Entities/Feedbacks.cs
+++ RestaurantApp.Entities/Feedbacks.cs
@@ -9,6 +9,7 @@ namespace RestaurantApp.Entities
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Comments { get; set; }
 
     }
--- RestaurantApp.Web/Controllers/HomeController.cs
+++ RestaurantApp.Web/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using RestaurantApp.Entities;
 using RestaurantApp.Web.Models;
 using System;
 using System.Collections.Generic;
@@ -37,6 +38,32 @@ namespace RestaurantApp.Web.Controllers
         public IActionResult About()
         {
             return View();
+        }
+
+
+        public  IActionResult Feedbacks()
+        {
+            return View(_context.Feedbacks.ToList());
+        }
+
+
+        public IActionResult NewFeedback()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult NewFeedback([Bind("Id,Comments")] Feedbacks feedbacks)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(feedbacks);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Feedbacks));
+            }
+            return View(feedbacks);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]

5a25674f5b8b3e20e72de0e66bc4e7346ae8e4da
Man Ioan <man_ioan@live.com>
2021-01-10 17:25:34 +0200

    Implement PayPal Integration for Shopping Cart

---
+++ RestaurantApp.Web/wwwroot/js/cart/paypal.js
@@ -0,0 +1,19 @@
+﻿$(function () {
+    $("a.placeorder").click(function (e) {
+        e.preventDefault();
+
+        var $this = $(this);
+
+        var url = "/cart/PlaceOrder";
+
+        $(".ajaxbg").show();
+
+        $.post(url, {}, function (data) {
+            $(".ajaxbg span").text("Thank you. You will now be redirected to paypal.");
+            setTimeout(function () {
+                $('form input[name="submit"]').click();
+            }, 2000);
+        });
+    });
+
+});

e9a49fbdb0e13f9c8279264fa796a1f7f7f0dc22
Man Ioan <man_ioan@live.com>
2021-02-12 21:05:16 +0200

    Add sql scripts

---
+++ RestaurantApp.Database/OrderedMenusTable.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[OrderedMenus](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[MenuId] [int] NOT NULL,
+	[OrderId] [int] NOT NULL,
+	[Quantity] [int] NOT NULL,
+	[OrdersOrderId] [int] NULL,
+ CONSTRAINT [PK_OrderedMenus] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
---
+++ RestaurantApp.Database/VouchersTable.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Vouchers](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](max) NOT NULL,
+	[VoucherType] [nvarchar](max) NOT NULL,
+	[Discount] [float] NOT NULL,
+	[MinimumAmount] [float] NOT NULL,
+	[IsActive] [bit] NOT NULL,
+ CONSTRAINT [PK_Vouchers] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO

a1ef8f9ef1cb32f8794a1586b319118973d0c60f
Man Ioan <man_ioan@live.com>
2021-02-18 20:19:16 +0200

    Implement CRUD & API for Vouchers entity

---
+++ RestaurantApp.Web/Controllers/API/VouchersApiController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantApp.Entities;
+using RestaurantApp.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantApp.Web.Controllers.API
+{
+    [Route("api/Vouchers")]
+    [ApiController]
+    public class VouchersApiController : ControllerBase
+    {
+        private readonly RestaurantDbContext _context;
+
+        public VouchersApiController(RestaurantDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("GetVouchers")]
+        public IActionResult GetVouchers()
+        {
+            var meals = _context.Vouchers;
+            return new OkObjectResult(meals);
+        }
+            
+        [HttpGet]
+        [Route("GetVoucherById")]
+        public IActionResult GetVoucherById(int id)
+        {
+            var voucher = from a in _context.Vouchers.Where(p => p.Id == id).ToList()
+                       select new
+                       {
+                           a.Id,
+                           a.Name,
+                           a.VoucherType,
+                           a.Discount,
+                           a.MinimumAmount,
+                           a.IsActive
+
+                       };
+            return new OkObjectResult(voucher);
+        }
+
+        [HttpPost]
+        [Route("CreateVoucher")]
+        public IActionResult CreateVoucher([FromBody] Vouchers voucher)
+        {
+            _context.Add(voucher);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(Vouchers), new { id = voucher.Id }, voucher);
+        }
+
+        [HttpPut]
+        [Route("UpdateVoucher")]
+        public IActionResult UpdateVoucher([FromBody] Vouchers voucher)
+        {
+            _context.Update(voucher);
+            _context.SaveChanges();
+            return new OkResult();
+        }
+
+        [HttpDelete]
+        [Route("DeleteVoucher")]
+        public IActionResult DeleteVoucher(int id)
+        {
+            var vouchers = _context.Vouchers.Find(id);
+            _context.Vouchers.Remove(vouchers);
+            _context.SaveChanges();
+            return new OkResult();
+        }
+    }
+}
---
+++ RestaurantApp.Web/Controllers/VouchersController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RestaurantApp.Entities;
+using RestaurantApp.Web.Models;
+
+namespace RestaurantApp.Web.Controllers
+{
+    [Authorize]
+    public class VouchersController : Controller
+    {
+        private readonly RestaurantDbContext _context;
+
+        public VouchersController(RestaurantDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Vouchers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Vouchers.ToListAsync());
+        }
+
+        // GET: Vouchers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vouchers = await _context.Vouchers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vouchers == null)
+            {
+                return NotFound();
+            }
+
+            return View(vouchers);
+        }
+
+        // GET: Vouchers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Vouchers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,VoucherType,Discount,MinimumAmount,IsActive")] Vouchers vouchers)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(vouchers);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vouchers);
+        }
+
+        // GET: Vouchers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vouchers = await _context.Vouchers.FindAsync(id);
+            if (vouchers == null)
+            {
+                return NotFound();
+            }
+            return View(vouchers);
+        }
+
+        // POST: Vouchers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,VoucherType,Discount,MinimumAmount,IsActive")] Vouchers vouchers)
+        {
+            if (id != vouchers.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(vouchers);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VouchersExists(vouchers.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vouchers);
+        }
+
+        // GET: Vouchers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vouchers = await _context.Vouchers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vouchers == null)
+            {
+                return NotFound();
+            }
+
+            return View(vouchers);
+        }
+
+        // POST: Vouchers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var vouchers = await _context.Vouchers.FindAsync(id);
+            _context.Vouchers.Remove(vouchers);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VouchersExists(int id)
+        {
+            return _context.Vouchers.Any(e => e.Id == id);
+        }
+    }
+}
